3512a83d3cfcbf822890e26f511187969e2c44f0
Dippi <dippi@altervista.org>
2016-08-12 11:36:49 +0200

    Extended testing initial thoughts

---
+++ tests/extended.rs
@@ -0,0 +1,30 @@
+// TODO Parameters:
+// * Rule
+//   - Num of predicates
+//   - Order of predicates
+// * Events
+//   - Frequency
+// * Static
+//   - Num of rows
+// * Data (both event and static)
+//   - Num of attributes / columns
+//   - Type of data
+//   - Data domain
+//   - Repetitions
+// * Events queries
+//   - Window
+// * Queries (both event and static)
+//   - Num of input params
+//   - Num of output params
+//   - Selection policy
+//   - Num of results
+//   - Selectivity (#propagated / #processed OR #results / #rows)
+//   - Filters complexity
+//   - Aggregates complexity
+// * Cache
+//   - Size
+//   - Type
+//   - Ownership
+//   - Ratio repeated vs new
+// * Other
+//   - Pre fetching
