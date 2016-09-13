fe11d4b50918dafba577b9a66120b3d1b345676e
Dippi <dippi@altervista.org>
2016-09-13 15:26:07 +0200

    Extended testing

--- tests/extended.rs
+++ tests/extended.rs
@@ -36,10 +36,10 @@
 
 #![feature(step_by, inclusive_range_syntax)]
 
-extern crate trex;
 extern crate chrono;
 extern crate rand;
 extern crate rusqlite;
+extern crate trex;
 
 use chrono::{Duration, UTC};
 use rand::Rng;
@@ -73,11 +73,13 @@ struct Config {
     db_name: String,
     table_columns: usize,
     table_rows: usize,
+    table_indexed: bool,
     cache_size: usize,
     cache_ownership: CacheOwnership,
     cache_type: CacheType,
     matching_rows: usize,
     matching_range: usize,
+    static_prob: f32,
 }
 
 fn setup_db<R: Rng>(rng: &mut R, cfg: &Config) {
@@ -111,12 +113,33 @@ fn setup_db<R: Rng>(rng: &mut R, cfg: &Config) {
             let reference: Vec<_> = data.iter().map(|it| it as &ToSql).collect();
             stmt.execute(&reference).unwrap();
         }
+
+        if cfg.table_indexed {
+            for i in 0..cfg.table_columns {
+                let create_query = format!("CREATE INDEX index_col{0} ON test (col{0})", i);
+                tx.execute(&create_query, &[]).unwrap();
+            }
+        }
     }
 
     tx.commit().unwrap();
 }
 
 fn generate_declarations<R: Rng>(rng: &mut R, cfg: &Config) -> Vec<TupleDeclaration> {
+    let attributes = (0..cfg.table_columns)
+        .map(|i| {
+            AttributeDeclaration {
+                name: format!("col{}", i),
+                ty: BasicType::Int,
+            }
+        })
+        .collect();
+    let static_decl = TupleDeclaration {
+        ty: TupleType::Static,
+        id: 0,
+        name: "test".to_owned(),
+        attributes: attributes,
+    };
     (0..cfg.num_def)
         .flat_map(|i| {
             let id = i + 1;
@@ -140,7 +163,7 @@ fn generate_declarations<R: Rng>(rng: &mut R, cfg: &Config) -> Vec<TupleDeclarat
                 name: format!("event{}", id * 1000),
                 attributes: attrs,
             };
-            let mid_decls = (1..(cfg.num_pred - 1)).map(move |j| {
+            let mid_decls = (1..cfg.num_pred).map(move |j| {
                 let attr = AttributeDeclaration {
                     name: "attr".to_owned(),
                     ty: BasicType::Int,
@@ -152,22 +175,9 @@ fn generate_declarations<R: Rng>(rng: &mut R, cfg: &Config) -> Vec<TupleDeclarat
                     attributes: vec![attr],
                 }
             });
-            let attributes = (0..cfg.table_columns)
-                .map(|i| {
-                    AttributeDeclaration {
-                        name: format!("col{}", i),
-                        ty: BasicType::Int,
-                    }
-                })
-                .collect();
-            let static_decl = TupleDeclaration {
-                ty: TupleType::Static,
-                id: id * 1000 + cfg.num_pred - 1,
-                name: "test".to_owned(),
-                attributes: attributes,
-            };
-            once(output_decl).chain(once(root_decl)).chain(mid_decls).chain(once(static_decl))
+            once(output_decl).chain(once(root_decl)).chain(mid_decls)
         })
+        .chain(once(static_decl))
         .collect()
 }
 
@@ -196,7 +206,43 @@ fn generate_rules<R: Rng>(rng: &mut R, cfg: &Config) -> Vec<Rule> {
                     alias: format!("alias{}", id * 1000),
                 },
             };
-            let mid_preds = (1..(cfg.num_pred - 1)).map(|j| {
+            let static_pred = if rng.next_f32() <= cfg.static_prob {
+                let static_constr1 = Arc::new(Expression::BinaryOperation {
+                    operator: BinaryOperator::GreaterEqual,
+                    left: Box::new(Expression::Reference { attribute: 0 }),
+                    right: Box::new(Expression::Parameter {
+                        predicate: 0,
+                        parameter: 0,
+                    }),
+                });
+                let static_constr2 = Arc::new(Expression::BinaryOperation {
+                    operator: BinaryOperator::LowerThan,
+                    left: Box::new(Expression::Reference { attribute: 0 }),
+                    right: Box::new(Expression::Parameter {
+                        predicate: 0,
+                        parameter: 1,
+                    }),
+                });
+                let parameters = (0..cfg.table_columns)
+                    .map(|i| {
+                        ParameterDeclaration {
+                            name: format!("z{}", i),
+                            expression: Arc::new(Expression::Reference { attribute: i }),
+                        }
+                    })
+                    .collect();
+                Some(Predicate {
+                    ty: PredicateType::UnorderedStatic { parameters: parameters },
+                    tuple: ConstrainedTuple {
+                        ty_id: 0,
+                        constraints: vec![static_constr1, static_constr2],
+                        alias: format!("alias{}", 0),
+                    },
+                })
+            } else {
+                None
+            };
+            let mid_preds = (1..cfg.num_pred).map(|j| {
                 let rand = rng.next_f32();
                 let selection = if rand < cfg.each_prob {
                     EventSelection::Each
@@ -224,39 +270,8 @@ fn generate_rules<R: Rng>(rng: &mut R, cfg: &Config) -> Vec<Rule> {
                     },
                 }
             });
-            let static_constr1 = Arc::new(Expression::BinaryOperation {
-                operator: BinaryOperator::GreaterEqual,
-                left: Box::new(Expression::Reference { attribute: 0 }),
-                right: Box::new(Expression::Parameter {
-                    predicate: 0,
-                    parameter: 0,
-                }),
-            });
-            let static_constr2 = Arc::new(Expression::BinaryOperation {
-                operator: BinaryOperator::LowerThan,
-                left: Box::new(Expression::Reference { attribute: 0 }),
-                right: Box::new(Expression::Parameter {
-                    predicate: 0,
-                    parameter: 1,
-                }),
-            });
-            let parameters = (0..cfg.table_columns)
-                .map(|i| {
-                    ParameterDeclaration {
-                        name: format!("z{}", i),
-                        expression: Arc::new(Expression::Reference { attribute: i }),
-                    }
-                })
-                .collect();
-            let static_pred = Predicate {
-                ty: PredicateType::UnorderedStatic { parameters: parameters },
-                tuple: ConstrainedTuple {
-                    ty_id: id * 1000 + cfg.num_pred - 1,
-                    constraints: vec![static_constr1, static_constr2],
-                    alias: format!("alias{}", id * 1000 + cfg.num_pred),
-                },
-            };
-            let predicates = once(root_pred).chain(mid_preds).chain(once(static_pred)).collect();
+
+            let predicates = once(root_pred).chain(mid_preds).chain(static_pred).collect();
             let event_template = EventTemplate {
                 ty_id: id,
                 attributes: Vec::new(),
@@ -276,7 +291,7 @@ fn generate_events<R: Rng>(rng: &mut R, cfg: &Config) -> Vec<Event> {
     (0..cfg.num_events)
         .map(|_| {
             let def = rng.gen_range(0, cfg.num_def) + 1;
-            let state = rng.gen_range(0, cfg.num_pred - 1);
+            let state = rng.gen_range(0, cfg.num_pred);
             if state == 0 {
                 let lower_bound = rng.gen_range(0, cfg.matching_range) as i32;
                 let upper_bound =
@@ -361,36 +376,36 @@ fn execute_bench(cfg: &Config) {
 #[test]
 fn extended_test() {
     println!("");
-    for freq in (5_000...5_000).step_by(2000) {
-        println!("- Frequency: {:5} evt/sec", freq);
-        for avg_win in (2...2).step_by(4) {
-            let max_win = Duration::seconds(avg_win + 1 as i64);
-            let min_win = Duration::seconds(avg_win - 1 as i64);
-            print!(" > Avg Window: {:2}s => ", avg_win);
+    let freq = 3_000;
+    println!("- Frequency: {:5} evt/sec", freq);
+    let avg_win = 2;
+    let max_win = Duration::seconds(avg_win + 1 as i64);
+    let min_win = Duration::seconds(avg_win - 1 as i64);
+    print!(" > Avg Window: {:2}s => ", avg_win);
 
-            let mut cfg = Config {
-                num_rules: 1000,
-                num_def: 100,
-                num_pred: 4,
-                num_events: 30_000,
-                each_prob: 1.0,
-                first_prob: 0.0,
-                min_win: min_win,
-                max_win: max_win,
-                consuming: false,
-                queue_len: 100,
-                evts_per_sec: freq,
-                db_name: "./database.db".to_owned(),
-                table_columns: 2,
-                table_rows: 100_000,
-                cache_size: 1000,
-                cache_ownership: CacheOwnership::PerPredicate,
-                cache_type: CacheType::Lru,
-                matching_rows: 10,
-                matching_range: 300,
-            };
+    let mut cfg = Config {
+        num_rules: 1000,
+        num_def: 100,
+        num_pred: 3,
+        num_events: 50_000,
+        each_prob: 1.0,
+        first_prob: 0.0,
+        min_win: min_win,
+        max_win: max_win,
+        consuming: false,
+        queue_len: 250,
+        evts_per_sec: freq,
+        db_name: "./database.db".to_owned(),
+        table_columns: 1,
+        table_rows: 100_000,
+        table_indexed: true,
+        cache_size: 250,
+        cache_ownership: CacheOwnership::PerPredicate,
+        cache_type: CacheType::Lru,
+        matching_rows: 10,
+        matching_range: 30,
+        static_prob: 0.2,
+    };
 
-            execute_bench(&cfg);
-        }
-    }
+    execute_bench(&cfg);
 }
